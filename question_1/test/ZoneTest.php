0a40ca4ff26d1a1613b1a271b1c5ea61013c5663
p33rs <jon@peers.pw>
2015-03-10 22:04:22 -0400

    example unit test

---
+++ question_1/test/ZoneTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admittedly, I am not great at writing tests, but I'd like to be.
+ */
+class ZoneTest extends PHPUnit_Framework_TestCase {
+    public function testExample()
+    {
+        $test = ['M3', 'R3-2', 'PARKNYS', 'M1-3/R9'];
+        $this->assertEquals(
+            [ 'codes' => [
+                [ 'code' => 'M3', 'description' => 'Not found' ],
+                [ 'code' => 'R3-2', 'description' => 'General Residence Districts' ],
+                [ 'code' => 'PARKNYS', 'description' => 'New York State Parks' ],
+                [ 'code' => 'M1-3/R9', 'description' => 'Mixed Manufacturing & Residential Districts' ],
+            ]],
+            Zone::parseCodes($test)
+        );
+    }
+}
